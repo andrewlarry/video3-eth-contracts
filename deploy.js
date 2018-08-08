92c869a9ebc682a16088a0575c4939dd53dd2a0c
Andrew <afuselier23@gmail.com>
2018-08-07 17:28:37 -0700

    refactor deploy script

--- deploy.js
+++ deploy.js
@@ -1,41 +1,42 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const fs = require('fs');
+const fs = require('fs-extra');
+const path = require('path');
 
-const { interface, bytecode } = require('./compile');
+// Path to build directory
+const buildPath = path.resolve(__dirname, 'build');
+
+const compiledContract = require('./build/HashStorage.json');
 
 // 12 word memonic for deriving accounts
 const mnemonic = require('./config/mnemonic');
 
 // Infura remote node enpoint
 const { TEST_ENDPOINT } = require('./config/infura');
-
 const provider = new HDWalletProvider(mnemonic, TEST_ENDPOINT);
 
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('account: ', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: `0x${bytecode}`, arguments: ['deploy'] })
+  const result = await new web3.eth.Contract(JSON.parse(compiledContract.interface))
+    .deploy({ data: `0x${compiledContract.bytecode}`})
     .send({ from: accounts[0], gas: '2000000' })
     .catch(err => console.log(err));
 
-  console.log('address: ', result.options.address);
-
+  // Store this data in a .json file for future reference
   const contractData = {
     account: accounts[0],
     address: result.options.address,
-    interface,
-    bytecode
+    interface: compiledContract.interface,
+    bytecode: compiledContract.bytecode
   }
 
-  fs.writeFile("/config/contract.json", contractData, 'utf8', err => {
-    if (err) return console.log(err);
-    console.log("The file was saved!");
-  }); 
+  fs.outputJsonSync(
+    path.resolve(buildPath, 'deployedHashStorage.json'),
+    contractData
+  );
 };
 
 deploy();
