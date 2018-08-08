88eccf6171f704ae4900ae015ccdcb49ceae2d05
Andrew <afuselier23@gmail.com>
2018-08-07 17:16:28 -0700

    refactor compile script to output json file

--- compile.js
+++ compile.js
@@ -1,10 +1,22 @@
 const path = require('path');
-const fs = require('fs');
+const fs = require('fs-extra');
 const { compile } = require('solc');
+
+// Get path to build directory and remove
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath)
 
 // Path to a smart contract
 const contractPath = path.resolve(__dirname, 'contracts', 'HashStorage.sol');
+
+// Read the contract file and compile with solc
 const source = fs.readFileSync(contractPath, 'utf8');
+const compiled = compile(source, 1).contracts;
 
-module.exports = compile(source, 1).contracts[':HashStorage'];
+// Rebuild the build directory
+fs.ensureDirSync(buildPath);
 
+fs.outputJsonSync(
+  path.resolve(buildPath, 'HashStorage.json'),
+  compiled[':HashStorage']
+);
