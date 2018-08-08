85e9c5329c8487476f676e6b8077f0fa8785c6a7
Andrew <afuselier23@gmail.com>
2018-08-07 17:43:57 -0700

    refactor tests

--- test/HashStorage.test.js
+++ test/HashStorage.test.js
@@ -5,40 +5,64 @@ const Web3 = require('web3');
 const provider = ganche.provider();
 const web3 = new Web3(provider);
 
-const { interface, bytecode } = require('../compile');
+const compiledContract = require('../build/HashStorage.json');
 
 let accounts;
-let hashStorage; 
-const INITIAL_HASH = 'abc123';
+let contract; 
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
-  // Use one of those accounts to deploy contract
-
-  // JSON.parse(interface) = JS object that represents the ABI
-  hashStorage = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: [INITIAL_HASH] })
+  // Deploy a contract with the first account, account[0]
+  contract = await new web3.eth.Contract(JSON.parse(compiledContract.interface))
+    .deploy({ data: compiledContract.bytecode })
     .send({ from: accounts[0], gas: '1000000' });
 
-  hashStorage.setProvider(provider);
+  contract.setProvider(provider);
 });
 
 describe('HashStorage', () => {
   it('Deploys a contract', () => {
-    // Fails if argument is null or undefined
-    assert.ok(hashStorage.options.address);
+    assert.ok(contract.options.address);
   });
 
-  it('Initializes hash with constructor argument', async () => {
-    const message = await hashStorage.methods.hash().call();
-    assert.equal(message, INITIAL_HASH);
+  it('Adds a video to the contract', async () => {
+    // Store a hash with the managers account
+    await contract.methods.storeHash('abc', '123', 'user1').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    // The index should now be equal to 2
+    const index = await contract.methods.index().call();
+
+    assert.equal(2, index);
   });
 
-  it('Sets a new hash with the setHash() method', async () => {
-    await hashStorage.methods.setHash('testing').send({ from: accounts[0] });
-    const hash = await hashStorage.methods.hash().call();
-    assert.equal(hash, 'testing');
+  it('Returns the user string from the contract', async () => {
+    // Store a hash with the managers account
+    await contract.methods.storeHash('abc', '123', 'user1').send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    // Call the getVideo method with the hash
+    const user = await contract.methods.getVideo('abc').call();
+
+    assert.equal('user1', user);
+  });
+
+  it('Only the manager can store a video', async () => {
+    try {
+      await contract.methods.storeHash('abc', '123', 'user1').send({
+        from: accounts[1],
+        gas: '1000000'
+      });
+      // Should not reach this line
+      assert(false);
+    } catch(err) {
+      assert(err);
+    }
   });
 });
